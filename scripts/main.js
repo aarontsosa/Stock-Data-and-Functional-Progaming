6a152501becc4cece8b2d1c26222b799b5879661
Aaron Sosa <aarontsosa@gmail.com>
2017-07-17 15:57:33 -0400

    added fuctional programing to take adverages, arrenge from high to low, and a daily report to the stock info

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,45 @@
-function averages(stocks){
-    days = Object.keys(stocks);
-    return stocks
-        .map((day) => {
-            return day.open
-        })
-        .reduce((open, value) =>{
-            return open + value
-        })
-    return stocks / days.length();
-}
+    function averages(stocks, values){
+        num = getNum(stocks, values)
+        num = num
+            .reduce((open, value) => {
+                return (Number(open) + Number(value))
+            })
+            / 100 
+        return num.toFixed(3)
+    }
+
+    function getNum(stocks, values){
+        days = Object.keys(stocks)
+        return days
+            .map((x) => {
+                return stocks[x][values]
+            })
+    }
+
+
+
+    function sorting(stocks, values){
+        values = getNum(stocks, values)
+        return values.sort()
+    }
+
+    function sortHigh(stocks){
+        x = sorting(stocks, "2. high")
+        return x.reverse()
+    }
+
+    function sortLow(stocks){
+        return sorting(stocks, "3. low")
+    }
+
+
+    function dayReport(stocks){
+        days = Object.keys(stocks)
+        days = days
+            .map((x) => {
+                return "Day: "+ x +" Open: " + stocks[x]["1. open"] + " Close: " + stocks[x]["4. close"] + "\n"
+            }) 
+        return console.log(days)
+        
+    }
+
