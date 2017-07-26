7b12a553190054884de55444f0012d6d41e5fda4
Aaron Sosa <aarontsosa@gmail.com>
2017-07-26 16:01:22 -0400

    Testing out API

---
+++ scripts/test.js
@@ -0,0 +1,20 @@
+var socket = io.connect('http://socket.coincap.io');
+
+function coinName(payload) {
+  let parent = document.querySelector('.container')
+  let elem = document.createElement('p')
+  elem.textContent = payload.message.coin
+  parent.appendChild(elem)
+}
+
+socket.on('trades', function (tradeMsg) {
+    // console.log(tradeMsg)
+    var BTC = tradeMsg["message"]["msg"]["long"]
+    var value = tradeMsg["message"]["msg"]["price"]
+    var i = 0
+    if (BTC = "Bitcoin"){
+        console.log(tradeMsg)
+        $('[data-BTC="target"]')[0].textContent = BTC + " " + value
+    }
+    // coinName(tradeMsg);
+})
