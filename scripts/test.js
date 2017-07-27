4acced277a71ffb653aa124d505aad6d9a330f53
Aaron Sosa <aarontsosa@gmail.com>
2017-07-27 10:07:17 -0400

    Live ticker with position being called rather then name

--- scripts/test.js
+++ scripts/test.js
@@ -9,12 +9,22 @@ function coinName(payload) {
 
 socket.on('trades', function (tradeMsg) {
     // console.log(tradeMsg)
-    var BTC = tradeMsg["message"]["msg"]["long"]
+    var CurName = tradeMsg["message"]["msg"]["long"]
     var value = tradeMsg["message"]["msg"]["price"]
+    var supply = tradeMsg["message"]["msg"]["supply"]
     var i = 0
-    if (BTC === "Bitcoin"){
-        console.log(tradeMsg)
-        $('[data-BTC="target"]')[0].textContent = BTC + " " + value
+    if (CurName === "Bitcoin"){
+        $('[data-name="target"]')[0].textContent = CurName
+        $('[data-price="target"]')[0].textContent = value
+        $('[data-supply="target"]')[0].textContent = supply
+    } else if (CurName === "Ethereum"){
+        $('[data-name="target"]')[1].textContent = CurName
+        $('[data-price="target"]')[1].textContent = value
+        $('[data-supply="target"]')[1].textContent = supply
+    } else if (CurName === "Ripple"){
+        $('[data-name="target"]')[2].textContent = CurName
+        $('[data-price="target"]')[2].textContent = value
+        $('[data-supply="target"]')[2].textContent = supply
     }
     // coinName(tradeMsg);
 })
